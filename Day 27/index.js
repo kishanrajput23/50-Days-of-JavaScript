de67a958460758c4480f70071cbb3a0df9edafb6
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-12-08 22:39:38 +0530

    Day 27 Solution

--- Day 27/index.js
+++ Day 27/index.js
@@ -1,32 +1,13 @@
 function getCount(str) {
-    const vowelSet = new Set()
-    vowelSet.add('a')
-    vowelSet.add('e')
-    vowelSet.add('i')
-    vowelSet.add('o')
-    vowelSet.add('u')
-
-    let vowelsCount = 0
-
-    let startingPoint = 0
-    let endingPoint = str.length - 1
-
-    while(startingPoint <= endingPoint){
-        if(startingPoint === endingPoint){
-            if(vowelSet.has(str.charAt(startingPoint).toLowerCase())) vowelsCount++
-            startingPoint++
-            endingPoint--
-            continue
-        }
-
-        if (vowelSet.has(str.charAt(startingPoint).toLowerCase())) vowelsCount++
-        if (vowelSet.has(str.charAt(endingPoint).toLowerCase())) vowelsCount++
-
-        startingPoint++
-        endingPoint--
-    }
-
-    return vowelsCount
+    let vowelsCount = 0;
+    str = str.toLowerCase();
+    // enter your magic here
+    let arr = str.split('');
+    arr.forEach((a) => {
+        if(a==='a' || a==='e' || a==='i' || a==='o' || a==='u')
+          vowelsCount++;
+    })
+    return vowelsCount;
 }
 
 console.log(getCount("abracadabra"));
